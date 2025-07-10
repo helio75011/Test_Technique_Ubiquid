68953f897cac3f92bfce5f6945b9cc0cf3621ff0
de Breyne <111784301+helio75011@users.noreply.github.com>
2025-07-10 17:29:51 +0200

    create controller create for the API

--- packages/api/src/features/jobs/router.js
+++ packages/api/src/features/jobs/router.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { list } from "./controllers/list";
+import { create } from './controllers/create';
 
 const JobsRouter = Router();
 
 JobsRouter.get("/list", list);
+JobsRouter.post('/', create);
 
 export default JobsRouter;
